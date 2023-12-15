64de5b7c97569c4510fd5a52ee48b41d7b178a31
ahammed3216 <ayyarilnavad@gmail.com>
2023-12-15 10:54:35 +0530

    Revert "Revert "code without smart pointer""
    This reverts commit f716cc8e938bb8d0e4e10128a371bfe7ea1f4c9f.

--- assignment1/width_binary.cpp
+++ assignment1/width_binary.cpp
@@ -6,35 +6,35 @@ class Node
 {
     public:
             int data;
-            unique_ptr<Node> leftptr;
-            unique_ptr<Node> rightptr;
-
+            Node* leftptr;
+            Node* rightptr;
+ 
             Node(int data)
             {
                 this->data=data;
                 this->leftptr=nullptr;
                 this->rightptr=nullptr;
             }
-
+ 
             void show_data()
             {
                 cout<<endl<<data;
             }
-
+ 
             ~Node()
             {
                 cout<<endl<<"\nDetructor is called !!";
             }
-
-
+ 
+ 
 };
-
-
+ 
+ 
 class binary_tree
 {
     private:
-        unique_ptr<Node>head;
-
+        Node *head;
+ 
     public:
         
         binary_tree()
@@ -42,22 +42,22 @@ class binary_tree
             this->head=nullptr;
             
         }
-
+ 
         void InsertData(int value)
         {
             cout<<"at the insert section :"<<value<<endl;
-            unique_ptr<Node> new_node(new Node(value));
+            Node* new_node=new Node(value);
             if(head ==nullptr)
             {
                 cout<<"\nhead value :"<<value<<endl;
-                head=move(new_node);
+                head=(new_node);
                 head->leftptr=nullptr;
                 head->rightptr=nullptr;
             }
             else
             {
                 
-                Node* temp=head.get();
+                Node* temp=head;
                 
                 while(true)
                 {
@@ -66,42 +66,41 @@ class binary_tree
                         if((temp->rightptr) == nullptr)
                         {
                             cout<<endl<<"Data is added ath the left"<<endl;
-                            temp->rightptr=move(new_node);
+                            temp->rightptr=new_node;
                             
                             return;
                         }
-                        temp=(temp->rightptr).get();
+                        temp=temp->rightptr;
                     }
-
+ 
                     else if (temp->data > value)
                     {
                         if(temp ->leftptr == nullptr)
                         {
                             cout<<endl<<"Data is added ath the right"<<endl;
-                            temp->leftptr=move(new_node);
+                            temp->leftptr=new_node;
                             
                             return;
                         }
-                        temp=(temp->leftptr).get();
+                        temp=temp->leftptr;
                     }
                     else
                     {
                         cout<<"In the else section";
-                        
+                        delete new_node;
                         return;
                     }
                 }
-
+ 
                 }
-
+ 
             }
-
-
+ 
+ 
         void DisplayData()
         {
             stack<Node*>values;
-            Node *temp;
-            temp=(this->head).get();
+            Node *temp=head;
             
             
             while(temp != nullptr || !values.empty())
@@ -109,26 +108,26 @@ class binary_tree
                 while(temp != nullptr)
                 {
                     values.push(temp);
-                    temp=(temp->rightptr).get();
+                    temp=temp->rightptr;
                    
-
+ 
                 }
-
+ 
                 temp=values.top();
                 values.pop();
                 cout<<temp->data<<" ";
                 
-                temp=(temp->leftptr).get();
+                temp=temp->leftptr;
                 
-
+ 
             }
         }
-
+ 
         void Depthvalue()
         {
             stack<Node*>values;
             int max=-1;
-            Node *temp=head.get();
+            Node *temp=head;
             int count=1;
             bool flag=true;
             
@@ -139,42 +138,41 @@ class binary_tree
                 {
                     flag=false;
                     values.push(temp);
-                    temp=(temp->rightptr).get();
+                    temp=temp->rightptr;
                     count++;
                    
-
+ 
                 }
                 if(flag==true)
                 {
-
+ 
                     cout<<"\nmaximum length is "<<count;
                     if(count>max)
                     {
                         max=count;
                     }
                 }
-
+ 
                 temp=values.top();
                 values.pop();
                 //cout<<temp->data<<" ";
                 
-                temp=(temp->leftptr).get();
+                temp=temp->leftptr;
                 count--;
-
+ 
             }
-            delete(temp);
-
+ 
             cout<<"\nmaximum depth value of the tree is"<<max;
         }
-
+ 
         
-
+ 
 };
-
+ 
 int main()
 {
     unique_ptr<binary_tree>obj_ptr(new binary_tree());
-
+ 
     obj_ptr->InsertData(5);
     obj_ptr->InsertData(3);
     obj_ptr->InsertData(7);
@@ -184,12 +182,12 @@ int main()
     obj_ptr->InsertData(8);
     obj_ptr->InsertData(9);
     obj_ptr->InsertData(10);
-
+ 
     obj_ptr->DisplayData();
-
+ 
     obj_ptr->Depthvalue();
-
-
+ 
+ 
     
-
+ 
 }

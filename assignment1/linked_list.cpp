fff0bf180f36257a3a8141406c2c6ea848937f1c
ahammed3216 <ayyarilnavad@gmail.com>
2023-12-18 17:11:01 +0530

    completed the linkedlist problems

--- assignment1/linked_list.cpp
+++ assignment1/linked_list.cpp
@@ -1,17 +1,25 @@
 #include<iostream>
 #include<list>
-#include<iterator>
+
 using namespace std;
 int main()
 {
+    clock_t start,end;
+    start=clock();
     list<int>list1;
-    list1.push_back(6);
-    list1.push_back(7);
-    list1.push_back(8);
-    list1.push_back(9);
-    list1.push_back(10);
-    
-   
+    // list1.push_back(6);
+    // list1.push_back(7);
+    // list1.push_back(8);
+    // list1.push_back(9);
+    // list1.push_back(10);
+    // list1.push_back(20);
+    // list1.push_back(30);
+   list1.emplace_back(6);
+   list1.emplace_front(7);
+   list1.emplace_back(3);
+   list1.emplace_back(1);
+   list1.emplace_front(10);
+   list1.emplace_back(11);
 
    list<int>::iterator slow=list1.begin();
    list<int>::iterator fast=list1.begin();
@@ -23,6 +31,7 @@ int main()
     }
 
     cout<<"the middle value is :"<<*slow;
-
-   
+    end=clock();
+    std::cout<<"Time:"<<(end-start)<<std::endl;
+    return 0;
 }

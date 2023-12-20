de98515db81bf28e37fce1616b98f02f610752d4
ahammed3216 <ayyarilnavad@gmail.com>
2023-12-20 12:32:18 +0530

    Revert "added the fil code with correct version"
    This reverts commit 98f99ff0c7573071a83d1b8a72a93bbfd4de8e37.

--- assignment1/matrix_fill.cpp
+++ assignment1/matrix_fill.cpp
@@ -1,56 +1,87 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
-vector<vector<int>> solve(vector<vector<int>>& matrix, int i, int j, int src, int color);
-
-int main() {
+vector<vector<int>> solve(vector<vector<int>>matrix,int row,int col,int src,int color);
+class Matrx
+{
+    private:
+        int row;
+        int col;
+        vector<vector<int>> matrix;
+        
+};
+int main()
+{
     int row, col, i = 0, j = 0;
-    cout << "Enter the row of the matrix: ";
+    cout << "Enter the row of the matrx :";
     cin >> row;
-    cout << "Enter the col of the matrix: ";
+    cout << "Enter the col of the matrx :";
     cin >> col;
-
     vector<vector<int>> matrix(row, vector<int>(col));
+    vector<vector<int>> matrix1(row, vector<int>(col));
 
-    for (i = 0; i < row; i++) {
-        for (j = 0; j < col; j++) {
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
             cin >> matrix[i][j];
         }
     }
 
     cout << "Printing the matrix :\n";
-    for (i = 0; i < row; i++) {
-        for (j = 0; j < col; j++) {
-            cout << matrix[i][j] << "\t";
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            cout << matrix[i][j]<<"\t";
         }
-        cout << endl;
+        cout<<endl;
     }
 
-    solve(matrix, 1, 1, matrix[1][1], 2);
+    //cout<<"check"<<matrix[0].size();
 
-    cout << "Printing the matrix after the sort:\n";
-    for (i = 0; i < row; i++) {
-        for (j = 0; j < col; j++) {
-            cout << matrix[i][j] << "\t";
+    matrix1=solve(matrix,1,1,1,2);
+
+     cout << "Printing the matrix after the sort:\n";
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            cout << matrix1[i][j]<<"\t";
         }
-        cout << endl;
+        cout<<endl;
     }
+
+    
 
     return 0;
 }
 
-vector<vector<int>> solve(vector<vector<int>>& matrix, int i, int j, int src, int color) {
-    if (i < 0 || j < 0 || i >= matrix.size() || j >= matrix[0].size() || matrix[i][j] != src) {
+vector<vector<int>> solve(vector<vector<int>>matrix,int i,int j,int src,int color)
+{
+   
+   if(i<0 || j<0 || i>=matrix.size() ||j>=matrix[0].size() || matrix[i][j] != src )
+   {
         return matrix;
+   }
+
+   matrix[i][j]=color;
+  
+   solve(matrix,i,j+1,src,color);
+   solve(matrix,i,j-1,src,color);
+   solve(matrix,i+1,j,src,color);
+   solve(matrix,i-1,j,src,color);
+   cout<<endl;cout<<endl;
+ for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            cout << matrix[i][j]<<"\t";
+        }
+        cout<<endl;
     }
+    cout<<endl;cout<<endl;
+   
+   
 
-    matrix[i][j] = color;
-
-    solve(matrix, i, j + 1, src, color);
-    solve(matrix, i, j - 1, src, color);
-    solve(matrix, i + 1, j, src, color);
-    solve(matrix, i - 1, j, src, color);
-
-    return matrix;
 }

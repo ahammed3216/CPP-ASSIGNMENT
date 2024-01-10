06062de9fc6856e59b2ba724ebba409600c5ef2e
ahammed3216 <ayyarilnavad@gmail.com>
2024-01-10 10:15:35 +0530

    added the task 38

---
+++ assignment1/task_38.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<vector>
+#include<memory>
+using namespace std;
+template<class T>
+class SortClass final
+{
+    private:
+        vector<T>nums;
+    public:
+        SortClass(vector<T>arr):nums(arr){}
+
+        void Sort()
+        {
+            int low=0,med=0,high=nums.size()-1;
+            
+
+            while(med<=high)
+            {
+                if (nums[med]==0)
+                {
+                    swap(nums[med],nums[low]);
+                    low++;
+                    med++;
+                }
+                else if(nums[med]==2)
+                {
+                    swap(nums[high],nums[med]);
+                    high--;
+                    med++;
+                }
+                else
+                {
+                    
+                }
+                
+            }
+
+
+        }
+
+        void display()
+        {
+            for(auto it =nums.begin();it!=nums.end();it++)
+            {
+                cout<<*it<<"\t";
+            }
+
+        }
+
+
+
+
+};
+int main()
+{
+    vector<int>arr={2,2,2,1,1,1};
+    unique_ptr<SortClass<int>>ptr=make_unique<SortClass<int>>(arr);
+    ptr->Sort();
+    ptr->display();
+}

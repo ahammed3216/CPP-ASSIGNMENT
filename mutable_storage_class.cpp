8684c045038862a55eda29d75612fe70899fd246
ahammed3216 <ayyarilnavad@gmail.com>
2024-01-11 13:12:13 +0530

    add minor change in mutable

--- mutable_storage_class.cpp
+++ mutable_storage_class.cpp
@@ -4,22 +4,31 @@ class Mutable
 {
     private:
         mutable int data;
+        int data1;
 
     public:
         Mutable(int x):data(x){}
 
         void update() const
         {
+            
+            data++; //when declare as const the updation is not possible
+            cout<<"the data is "<<data<<endl;
+        }
+
+        void update1() 
+        {
+            data1++;
             data++; //when declare as const the updation is not possible
             cout<<"the data is "<<data<<endl;
         }
 };
 int main()
 {
-     const Mutable obj(5);
-     Mutable obj2(4);
+      Mutable obj(5);
+     const Mutable obj2(4);
     // obj.update(); will cause error
-    obj2.update();
+    obj2.update1();
     obj.update();
 
      
